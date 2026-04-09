ab64f05f6a74d92ebefd9c6a22725489094086e4
rubyramirez88 <ruizruby160@gmail.com>
2026-04-09 11:24:21 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by RAMIREZ, RUBY on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
